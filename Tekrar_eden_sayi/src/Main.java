7c46fc87c8bcb0a4d7ed1af9d7cfd64b7efecd98
Hande Fettahoğlu <fettahogluhande@gmail.com>
2023-07-05 14:49:49 +0300

    initial commit

---
+++ Tekrar_eden_sayi/src/Main.java
@@ -0,0 +1,68 @@
+public class Main {
+    /*
+    static boolean isFind(int[] arr, int value) {
+
+        for (int i : arr) {
+            if (i == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 3, 8, 2, 2, 7, 3};
+        int[] duplicate = new int[arr.length];
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if ((i != j) && (arr[i] == arr[j])) {
+                    if (!isFind(duplicate, arr[i])) {
+                        duplicate[count++] = arr[i];
+                        break;
+                    }
+                }
+            }
+        }
+        for (int value : duplicate) {
+            if (value != 0) {
+                System.out.println(value);
+            }
+        }
+
+    }
+     */
+    static boolean isFind(int[] arr, int value) {
+
+        for (int i : arr) {
+            if (i == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 3, 8, 2, 2, 7, 4,4,3};
+        int[] duplicate = new int[arr.length];
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if ((i != j) && (arr[i] == arr[j]) && arr[i]%2==0) {
+                    if (!isFind(duplicate, arr[i])) {
+                        duplicate[count++] = arr[i];
+                        break;
+                    }
+                }
+            }
+        }
+        for (int value : duplicate) {
+            if (value != 0) {
+                System.out.println(value);
+            }
+        }
+
+    }
+}
